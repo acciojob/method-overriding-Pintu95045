c4746762de96b650f54487fbce196b3a3f090068
pintu_giri <mrpintugiri@gmail.com>
2024-02-12 23:17:18 +0530

    solution added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
+import com.myClasses.B;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        // 3rd statement
+        B b=new B();
+        System.out.println(b.call());
+        System.out.println(b.meth());
+    }
   
 }
---
+++ src/main/java/com/myClasses/A.java
@@ -0,0 +1,7 @@
+package com.myClasses;
+
+public class A {
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
